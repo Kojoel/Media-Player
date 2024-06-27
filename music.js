91858e0c24d9ba5766bb81f38ca2ea2fdf7e38f5
IT Compliance <compliance@amalitech.org>
2024-06-27 09:45:36 -0700

    Added Functionality: play duration, player updates

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 const musicListContainer = document.querySelector('.music-list-container');
 const playerWrapper = document.querySelector('.player-wrapper');
 const playerBg = document.querySelector('.player-dark-bg');
-
+let currentlyPlaying = null;
 // console.log(musicListContainer);
 
 const url = 'songs.json';
@@ -29,38 +29,73 @@ async function getSongCards() {
         musicListContainer.insertAdjacentHTML('beforeend', cardData);
 
         const musicList = musicListContainer.querySelectorAll('.music-card');
-        musicList.forEach(card => {
-            card.addEventListener('click', (e) => {
+        // const audioList = musicListContainer.querySelectorAll('#audio');
+        const progressBar = document.querySelector('#progress');
+        const songTitle = document.querySelector('#song-title');
+        const TimeCurrently = document.querySelector('#current-time');
+        const TotalTime = document.querySelector('#total-time');
+
+        musicList.forEach((card) => {
+            card.addEventListener('click', () => {
                 // console.log(card.innerText);
                 playerBg.style.display = "flex";
                 playerWrapper.style.display = "flex";
 
-                //Customizing music player
-                const progressBar = document.getElementById("#progress");
-                const audios = card.querySelector('audio');
-                audios.forEach(audio => {
-                    console.log(audio);
-                })
+                // console.log(card);
+                setInterval(() => {
+                    const audios = card.querySelector('audio');
+                    audios.play();
+                    // console.log(audios.currentTime);
+                    progressBar.max = audios.duration;
+                    progressBar.value = audios.currentTime;
+
+                    const songName = card.querySelector('#song-name');
+                    const artistName = card.querySelector('#artist-name');
+                    songTitle.textContent = songName.textContent + "   " + artistName.textContent;
+
+                    //Gets song total play time in format MM:SS
+                    const playTime = function () {
+                        this.minutes = Math.floor(audios.duration/60);
+                        this.seconds = Math.floor(audios.duration%60);
+                        this.total = `${this.minutes} : ${this.seconds}`
+                        return this.total;
+                    }
+
+                    //Gets song current play time in format MM:SS
+                    const currentPlayTime = function () {
+                        this.minutes = Math.floor((progressBar.value)/60);
+                        this.seconds = Math.floor((progressBar.value)%60);
+                        this.total = `${this.minutes} : ${this.seconds}`;
+                        return this.total;
+                    }
+
+                    //Displays the current time and total time
+                    TotalTime.textContent = playTime();
+                    // console.log(TotalTime.textContent);
+                    TimeCurrently.textContent = currentPlayTime();
+                    // console.log(TimeCurrently.textContent);
+                    
+                    const playPause = document.querySelector('.middle-circle');
+
+                    // playPause.addEventListener('click', (e) => {
+                    //     let isRunning = true;
+                    //     if(isRunning === true) {
+                    //         audios.pause();
+                    //         isRunning = false;
+                    //     }else{
+                    //         audios.play();
+                    //         isRunning = true;
+                    //     }
+                    // });
+
+                }, 1000);
+                
             });
-        })
+        });
 
     });   
 }
 getSongCards();
-
-
-// Music Player Customization
-
-
-// musicListContainer.addEventListener('click', (e) => {
-//     if(e.target && e.target.matches('.music-card')){
-//         console.log(e.textContent);
-//     }
-//     // playerBg.style.display = "flex";
-//     // playerWrapper.style.display = "flex";
-//     // console.log(e.target.parentNode.parentNode);
-//     // // console.log(musicListContainer.children);
-// })
 
 playerBg.addEventListener('click', () => {
     playerWrapper.style.display = "none";
