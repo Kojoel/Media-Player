583d7b4ddf8912f0f54c41908dfafdc198b88df7
Kojoel <nanaabruquah1s@gmail.com>
2024-07-01 07:34:40 +0000

    Video player feature implemented

--- appearance.js
+++ appearance.js
@@ -1,41 +1,44 @@
-// window.addEventListener('load', () => {
-//     const introWrapper = document.querySelector(".intro-wrapper");
-//     const optionWrapper = document.querySelector(".option-wrapper");
+window.addEventListener('load', () => {
+    const introWrapper = document.querySelector(".intro-wrapper");
+    const optionWrapper = document.querySelector(".option-wrapper");
 
-//     // Show introWrapper on load
-//     introWrapper.style.display = "flex";
+    // Show introWrapper on load
+    introWrapper.style.display = "flex";
 
-//     // Hide introWrapper and show optionWrapper after 1 second
-//     setTimeout(() => {
-//         introWrapper.style.display = "none";
-//         optionWrapper.style.display = "flex";
-//     }, 1000);
-// });
+    // Hide introWrapper and show optionWrapper after 1 second
+    setTimeout(() => {
+        introWrapper.style.display = "none";
+        optionWrapper.style.display = "flex";
+    }, 2000);
+});
 
-// const optionM = document.querySelector('.optionM');
-// const optionV = document.querySelector('.optionV');
-// const optionWrapper = document.querySelector(".option-wrapper");
-// const musicWrapper = document.querySelector('.music-wrapper');
-// const playerDarkBg = document.querySelector('.player-dark-bg');
-
-// optionM.addEventListener('click', (e) => {
-//     e.stopPropagation();
-//     console.log("clicked");
-//     optionWrapper.style.display = "none";
-//     // Show musicWrapper and wrapperForPlayer
-//     musicWrapper.style.display = "flex";
-// });
-
-// optionV.addEventListener('click', () => {
-//     // Assuming you want similar behavior for optionV
-//     optionWrapper.style.display = "none";
-//     musicWrapper.style.display = "none";
-// });
-
-const introWrapper = document.querySelector(".intro-wrapper");
+const optionM = document.querySelector('.optionM');
+const optionV = document.querySelector('.optionV');
 const optionWrapper = document.querySelector(".option-wrapper");
 const musicWrapper = document.querySelector('.music-wrapper');
+const playerDarkBg = document.querySelector('.player-dark-bg');
+const videoWrapper = document.querySelector('.video-wrapper')
 
-introWrapper.style.display = "none";
-optionWrapper.style.display = "none";
-musicWrapper.style.display = "none";
+optionM.addEventListener('click', (e) => {
+    e.stopPropagation();
+    console.log("clicked");
+    optionWrapper.style.display = "none";
+    // Show musicWrapper and wrapperForPlayer
+    musicWrapper.style.display = "flex";
+    videoWrapper.style.display = "none";
+});
+
+optionV.addEventListener('click', (e) => {
+    e.stopPropagation();
+    optionWrapper.style.display = "none";
+    musicWrapper.style.display = "none";
+    videoWrapper.style.display = "flex";
+});
+
+// const introWrapper = document.querySelector(".intro-wrapper");
+// const optionWrapper = document.querySelector(".option-wrapper");
+// const musicWrapper = document.querySelector('.music-wrapper');
+
+// introWrapper.style.display = "none";
+// optionWrapper.style.display = "none";
+// musicWrapper.style.display = "none";
---
+++ video.js
@@ -0,0 +1,38 @@
+
+const videos = document.querySelector('.list');
+const vidSrc = document.querySelector('video source');
+const video = document.querySelector('#myVideo');
+// console.log(vidSrc);
+// console.log(videos);
+
+const videoUrls = [
+    "video-samples/Life is fragile _ Reduce speed _ motivational video 👍.mp4"
+]
+
+const videoTitles = [
+    "Life is fragile_Reduce Speed",
+]
+
+
+function getVideoList() {
+    videoTitles.map((title) => {
+        const parTitle = document.createElement('p');
+        parTitle.textContent = title;
+
+        console.log(parTitle);
+        parTitle.classList.add('video-title');
+        videos.appendChild(parTitle);
+    })
+}
+getVideoList();
+
+
+// Playing video after click on parTitle
+const getVideos = document.querySelectorAll('.video-title');
+getVideos.forEach((titleElement, index) => {
+    titleElement.addEventListener('click', () => {
+        vidSrc.src = videoUrls[index];
+        video.load(); 
+        video.play();  
+    });
+});
